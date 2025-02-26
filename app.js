a08271b637a5a65d96ef6cc02f6395bdeab7ae9b
SAAAM485 <143632382+SAAAM485@users.noreply.github.com>
2025-02-26 18:59:49 +0800

    <Update> CSS applied.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const methodOverride = require("method-override");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 const albumRouter = require("./routes/albumRouter");
@@ -7,6 +8,7 @@ const albumRouter = require("./routes/albumRouter");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public"))); // 確保這一行存在
 app.use("/", albumRouter);
 
 app.listen(port, () => {
